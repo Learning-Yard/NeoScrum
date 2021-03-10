b29700def5bdf6b73e2c9078b03f76a33d353891
Anush02198 <Anush.4376@gmail.com>
2021-03-10 12:00:37 +0530

    Added Validation For Login Page

---
+++ src/app/login.ts
@@ -0,0 +1,6 @@
+export class Login {
+    constructor(
+        public e_name: string,
+        public e_email:string
+    ){}
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Login } from '../login'
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   // isLoggedIn:boolean = false;
-
+  
+  userModel = new Login ("","");
   constructor() { }
  
   ngOnInit(): void {
